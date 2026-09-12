767315613b59a29f2f46d1c605ba0bf6b26a7983
tkhan-09 <tkhan.ruet@gmail.com>
2026-09-12 16:41:40 +0600

    Technologies Header section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Footer from "./component/FooterSection";
 import Hero from "./component/HeroSection";
 import Navbar from "./component/Nav";
+import TechnologySectionHeader from "./component/Technologies/TechnologySectionHeader";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <TechnologySectionHeader/>
       <Footer />
     </>
   );
---
+++ src/component/Technologies/TechnologySectionHeader.tsx
@@ -0,0 +1,8 @@
+export default function TechnologySectionHeader(){
+    return(
+        <div className="container mx-auto px-4">
+            <h2 className="text-4xl font-black">Explore the <span className="bg-linear-to-r from-orange-500 to-pink-600 bg-clip-text text-transparent">Technologies</span></h2>
+            <p className="text-slate-500 leading-12 mb-8">Pick one technology per category to build your ideal stack</p>
+        </div>
+    )
+}
